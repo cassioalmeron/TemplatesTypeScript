66bcf57f50f10e8c54e34ffd43ed16a11fd08312
Cassio Almeron <cassioalmeron@gmail.com>
2021-11-23 16:47:11 -0300

    Add port to environment

--- backend-simple/src/server.ts
+++ backend-simple/src/server.ts
@@ -12,4 +12,6 @@ app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3333, () => console.log(`Server is running on port 3333!`));
+app.listen(process.env.APP_PORT, () =>
+  console.log(`Server is running on port ${process.env.APP_PORT}!`),
+);
